109afe934d9e44a628e90ad4b49ade306afeb972
mohagussetiaone <mohagussetiaone@gmail.com>
2022-07-01 16:07:16 +0700

    Membuat axios untuk menarik data restAPI third party TMDB

--- src/App.js
+++ src/App.js
@@ -9,11 +9,17 @@ function App() {
     // menggunakan theme seperti context
     // lakukan dengan inject context theme didalam sini
     <ThemeProvider theme={theme}>
+      {/* deklarasi div dengan nama class App */}
       <div className="App">
+        {/* deklarasi tag header dengan nama class App-Header */}
         <header className="App-header">
+          {/* mendeklarasikan component dari Box  yang mempunyai atribute p (dapat dibaca pada dokumentasi MUI) */}
           <Box p={2}>
+            {/* mendeklarasikan Typography untuk penulisan (dapat dilihat pada dokumentasi MUI) */}
             <Typography variant="h5">React List dan Axios</Typography>
+            {/* Tag penutup Box */}
           </Box>
+          {/* membuat box kembali dengan atribute sx yakni menerima value untuk p=> padding, display untuk tipe flex dan mempunyai gap 2 */}
           <Box
             sx={{
               p: 2,
@@ -21,20 +27,25 @@ function App() {
               gap: 2,
             }}
           >
+            {/* membuat component Button dengan atribute variant (dapat dilihat pada dokumentasi MUI, yang mempunyai atribute color dengan primary (yang diambil dari theme.js dan dapat bekerja karena sudah terimport)) */}
             <Button variant="contained" color="primary">
               Halo Warna Hijau
             </Button>
-
+            {/* membuat component Button dengan atribute variant (dapat dilihat pada dokumentasi MUI, yang mempunyai atribute color dengan secondary (yang diambil dari theme.js dan dapat bekerja karena sudah terimport)) */}
             <Button variant="contained" color="secondary">
               Halo Warna Biru
             </Button>
           </Box>
+          {/* Penutup tag header */}
         </header>
         {/* Membuat isi dari container */}
+        {/* penutup dari section */}
         <section>
+          {/* memuat component ListMovies pada folder src => component */}
           <ListMovies />
         </section>
       </div>
+      {/* tag penutup Theme Provider */}
     </ThemeProvider>
   );
 }
--- src/containers/ListMovies.jsx
+++ src/containers/ListMovies.jsx
@@ -1,19 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 // import MUI
-import { Box, Typography } from "@mui/material";
+import { accordionSummaryClasses, Box, Typography } from "@mui/material";
 // import css dari ListMovies.css
 import "./ListMovies.css";
 // menggunakan json secara langsung
 import jsonMovies from "../data/movies.json";
 import CardMovie from "../components/CardMovie";
+// import axios
+import axios from "axios";
 
 const ListMovies = () => {
+  // membungkus dalam try catch block
+  // membuat state
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    // didalam sini kita akan menggunakan axios
+    // membuat dalam bentuk async await dalam bentuk function lagi
+    const fetchDataMovies = async () => {
+      // membungkus dalam try cacth untuk penanganan error
+      try {
+        // jika menggunakan async await maka axios.get kita gunakan sebagai variabel
+        // pada https://api.themoviedb.org/3 adalah base dari urlnya
+        // pada movie/popular merupakan end point yang mempunyai method GET (dapat dilihat pada dokumentasi pemiliknya)
+        const response = await axios.get("https://api.themoviedb.org/3/movie/popular?api_key=63c2d7a15b4bfa0cf662f2bd615f10e0");
+        // untuk mendapatkan data maka kita gunakan response.data.results (dalam json)
+        setMovies(response.data.results);
+      } catch (err) {
+        console.log("Error nih", err);
+      }
+    };
+
+    // memanggil (invoke) function fecth data supaya dapat menjalankan useEf
+    fetchDataMovies();
+  });
+
   return (
     <Box className="boxy">
       <Typography variant="h5">Container for ListMovies</Typography>
       {/* Menggunakan suatu looping */}
       {/* menerima fungsi map dengan parameter movie */}
-      {jsonMovies.results.slice(-3).map((movie) => {
+      {/* dari variabel state kita mappingkan */}
+      {movies.map((movie) => {
         // return component jangan lupa untuk menggunakan key sebagai syarat
         return <CardMovie key={movie.id} propsMovie={movie} />;
       })}
